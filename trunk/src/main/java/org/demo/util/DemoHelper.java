d0254aeada5a424c137f6aea1ac9ab3696e5b7b2
send2r@gmail.com <send2r@gmail.com@702c119c-7826-11de-b03f-d78e5a28ce11>
2013-07-19 17:55:45 +0000

    1) added eclipse nature 2) updated pom XML to 	a) use newer libraries so that the code works (Again) 	b) added exec:java to execute from maven 3) changed most of the back end classes to incorporate change in libraries.

--- trunk/src/main/java/org/demo/util/DemoHelper.java
+++ trunk/src/main/java/org/demo/util/DemoHelper.java
@@ -5,22 +5,19 @@
 package org.demo.util;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Observable;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.Query;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.queryParser.MultiFieldQueryParser;
 import org.apache.lucene.queryParser.ParseException;
 import org.apache.lucene.queryParser.QueryParser;
 import org.apache.lucene.search.BooleanClause;
@@ -30,9 +27,9 @@ import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.PrefixQuery;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.WildcardQuery;
+import org.apache.lucene.util.Version;
 import org.demo.model.Song;
 import org.hibernate.Session;
-import org.hibernate.ejb.Ejb3Configuration;
 import org.hibernate.search.FullTextSession;
 import org.hibernate.search.Search;
 import org.hibernate.search.jpa.FullTextEntityManager;
@@ -48,19 +45,17 @@ public class DemoHelper extends Observable {
     public static final String[] QUERY_TYPES = {"Multifield", "Wildcard", "Term", "Prefix", "Fuzzy", "Match-all"};
     public static final String[] QUERY_TYPES_DESC = {"Query all fields in one go<br>For example, if you want to retrieve songs that has title Ticket<br>or any field containing text LA,<br> you could use the following <i>title: \\\"Ticket\\\" \\\"LA\\\"</i>",
         "Queries with the help of two wildcard symbols '*' (multiplecharacters) and '?' (single character). <br>These allow to match any character.<br>Example, <i>b*ll</i> will match ball,bull or bell..",
-        "It searches for a single term in a single field. Example, to retrieve all songs by Artist The Beatles, choose Artist field and search, <i>the beatles</i>",
+        "It searches for a single term in a single field. Example, to retrieve all songs by Artist Mohd Rafi, choose Artist field and search, <i>mohd rafi</i>",
         "A WildcardQuery that starts with characters and ends with the '*' symbol., Again, requires to choose a field to begin",
-        "Queries using the Levenshtein distance between terms.<br>Requires a minimum similarity float value that expands or<br>contracts the distance.<br> For example, if you want to Search albums that sounds like builder, choose field Album and search for <i>builder</i> this will return 'builder and boulder'",
+        "Queries using the Levenshtein distance between terms.<br>Requires a minimum similarity float value that expands or<br>contracts the distance.",
         "Returns all documents contained in a specified index."
     };
-    static final int RECS_TO_LOAD = 13;
+    static final int RECS_TO_LOAD = 18;
     private int percentageLoaded;
     private static final Log LOG = LogFactory.getLog(DemoHelper.class);
 
     private DemoHelper() {
-        Ejb3Configuration configuration = new Ejb3Configuration();
-        configuration.configure("music-db", new HashMap(1));
-        entityManagerFactory = configuration.buildEntityManagerFactory();
+        entityManagerFactory = Persistence.createEntityManagerFactory("music-db");
     }
 
     public static DemoHelper getDemoHelper() {
@@ -69,7 +64,6 @@ public class DemoHelper extends Observable {
 
     public List<Song> getAllRecords() {
         LOG.info("Returning all records using Match all query");
-        org.apache.lucene.search.Query luceneQuery = new MatchAllDocsQuery();
         EntityManager em = entityManagerFactory.createEntityManager();
         List<Song> list = em.createQuery("select s from Song s").getResultList();
 
@@ -153,12 +147,11 @@ public class DemoHelper extends Observable {
                 q = createHbMatchAllQuery();
 
         }
-        LOG.info("Query is of type " + q.getClass());
         return q.getResultList();
     }
 
     private Query createHbFuzzyQuery(String field, String userInput) {
-        FuzzyQuery fq = new FuzzyQuery(new Term(field, userInput), 0.4F);
+        FuzzyQuery fq = new FuzzyQuery(new Term(field, userInput));
         EntityManager em = entityManagerFactory.createEntityManager();
         FullTextEntityManager ftEm = org.hibernate.search.jpa.Search.getFullTextEntityManager(em);
         return ftEm.createFullTextQuery(fq, Song.class);
@@ -199,12 +192,7 @@ public class DemoHelper extends Observable {
     }
 
     private Query createHbMultiFieldSearchQuery(String searchText) throws ParseException {
-        Map<String, Float> weightMap = new HashMap<String, Float>(1);
-        /*weightMap.put("title", 5F);
-        weightMap.put("artist", 5F);
-        weightMap.put("album", 5F);
-        weightMap.put("notes", 5F);*/
-        QueryParser queryParser = new MultiFieldQueryParser(songFields, new StandardAnalyzer());
+        QueryParser queryParser = new org.apache.lucene.queryParser.MultiFieldQueryParser(Version.LUCENE_35,songFields,new StandardAnalyzer(Version.LUCENE_35));
         queryParser.setDefaultOperator(QueryParser.OR_OPERATOR);
         org.apache.lucene.search.Query lQuery = queryParser.parse(searchText);
         EntityManager manager = entityManagerFactory.createEntityManager();
